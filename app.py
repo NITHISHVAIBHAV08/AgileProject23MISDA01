34772a217168d461919fb0d8480e8468450be9c4
NITHISHVAIBHAV08 <nithishvaibhav.e2023@vitstudent.ac.in>
2026-01-20 15:10:46 +0530

    Ver1

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+print("Welcome to the E-Commerce Application")
+
+def main_menu():
+    print("\n1. View Products")
+    print("2. Login")
+    print("3. View Cart")
+    print("4. Checkout")
+    print("5. Exit")
+
+main_menu()
